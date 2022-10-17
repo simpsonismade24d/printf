365466c184eaa43eaebe7a364fa6b2d035507f4a
simpsonismade24d <simpsonismade@gmail.com>
2022-10-17 05:45:07 -0700

    Our printf Project

--- _printf.c
+++ _printf.c
@@ -1,114 +1,67 @@
-/*
- * File: _printf.c
- * Auth: Simpson Iluyomade
- *       Olalekan Samson Agboola
- */
-
 #include "main.h"
 
-/**
- * count_one_bits - Counts the number of bits set
- *                  to one in a binary number.
- * @num: The binary number.
- *
- * Return: The number of bits set to one.
- */
-unsigned char count_one_bits(unsigned char num)
-{
-	unsigned char count = 0;
-
-	while (num != 0)
-	{
-		if ((num & 1) == 1)
-			count++;
-		num >>= 1;
-	}
-
-	return (count);
-}
-
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
- */
-void cleanup(va_list args, buffer_t *output)
-{
-	va_end(args);
-	write(1, output->start, output->len);
-	free_buffer(output);
-}
-
-
-/**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
- *
- * Return: The number of characters stored to output.
- */
-int run_printf(const char *format, va_list args, buffer_t *output)
-{
-	int i, ret = 0;
-	char wid, prec, tmp;
-	unsigned char flag, len;
-	unsigned int (*f)(va_list, buffer_t *,\
-			unsigned char, char, char, unsigned char);
-
-	for (i = 0; *(format + i); i++)
-	{
-		len = 0;
-		if (*(format + i) == '%')
-		{
-			flag = handle_flags(format + i + 1);
-			tmp = count_one_bits(flag);
-			wid = handle_width(args, format + i + tmp + 1, &tmp);
-			prec = handle_precision(args, format + i + tmp + 1, &tmp);
-			len = handle_length(format + i + tmp + 1);
-			tmp += (len != 0) ? 1 : 0;
-			f = handle_specifiers(format + i + tmp + 1);
-			if (f != NULL)
-			{
-				i += tmp + 1;
-				ret += f(args, output, flag, wid, prec, len);
-				continue;
-			}
-			else if (*(format + i + tmp + 1) == '\0')
-			{
-				ret = -1;
-				break;
-			}
-		}
-		ret += _memcpy(output, (format + i), 1);
-		i += (len != 0) ? 1 : 0;
-	}
-	cleanup(args, output);
-	return (ret);
-}
-
-/**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- * Return: The number of characters printed.
- */
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
+* author: Simpson Iluyomade and Olalekan Samson Agboola
+*/
 int _printf(const char *format, ...)
 {
-	buffer_t *output;
-	va_list args;
-	int ret;
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
-	output = init_buffer();
-	if (output == NULL)
-		return (-1);
+if (format == NULL)
+return (-1);
 
-	va_start(args, format);
+va_start(list, format);
 
-	ret = run_printf(format, args, output);
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+rinted_chars++;
+}
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
 
-	return (ret);
+print_buffer(buffer, &buff_ind);
+
+va_end(list);
+
+return (printed_chars);
+}
+
+/**
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+
+*buff_ind = 0;
 }
